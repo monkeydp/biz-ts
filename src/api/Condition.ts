f59eb4fef9c49ed71e5b4b19dcddf2d3f765c5eb
iPotato-Work <monkeydpotato@gmail.com>
2020-10-27 15:07:15 +0800

    *

--- src/api/Condition.ts
+++ src/api/Condition.ts
@@ -19,7 +19,6 @@ export interface Where {
 export enum Operator {
     EQ = "EQ",      // Equal to
     GT = "GT",      // Greater than
-
     LT = "LT",      // Less than
     GTE = "GET",    // Greater than or equal to
     LTE = "LET",    // Less than or equal to
