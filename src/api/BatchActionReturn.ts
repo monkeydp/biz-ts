f77c2b3042f65b3e5e585ee78bd061a8aa9f6351
iPotato-Work <monkeydpotato@gmail.com>
2020-11-28 16:36:40 +0800

    *

--- src/api/BatchActionReturn.ts
+++ src/api/BatchActionReturn.ts
@@ -1,6 +1,14 @@
 import ValidError from "./ValidError";
 
+export enum BatchResult {
+    NO_DATA = "NO_DATA",
+    SUCCESS = "SUCCESS",
+    FAIL = "FAIL",
+    PARTIAL_FAIL = "PARTIAL_FAIL"
+}
+
 export default interface BatchActionReturn {
     validErrorsMap: Map<unknown, Array<ValidError>>;
     successData: Array<unknown>;
+    result: BatchResult;
 }
